0d4edf8a677c6d0aa586d78ae456dcffd9fc19c8
Paul <paulmlee7@gmail.com>
2018-02-12 16:05:55 -0800

    completed server and client setup. api setup complete

--- client/src/components/api_test.js
+++ client/src/components/api_test.js
@@ -1,10 +1,31 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class ApiTest extends Component {
+
+    getUserData(){
+        axios.get('/api/get-user').then((response) => {
+            console.log('response from get-user:', response);
+        }).catch((error) => {
+            console.log('error from get-user:', error);
+        })
+    }
+
+    getArticleData(){
+        axios.get('/api/get-articles').then(response => {
+            console.log('resp from articles:', response)
+        }).catch(error => {
+            console.log('error from get:', error.message)
+        })
+    }
+
     render(){
         return(
             <div>
                 <h1>API TESTING GROUNDZZ</h1>
+                <button onClick={this.getUserData}>get user info</button>
+                <br/>
+                <button onClick={this.getArticleData}>get article DATA</button>
             </div>
         )
     }
